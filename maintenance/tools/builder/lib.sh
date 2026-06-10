65484713d962e7f1ddd42ce5012350b3b0298552
PedroHLC <root@pedrohlc.com>
2026-06-10 10:19:07 -0300

    ci/build: don't fail while no-opping

--- maintenance/tools/builder/lib.sh
+++ maintenance/tools/builder/lib.sh
@@ -237,21 +237,19 @@ function deploy() {
 
     # Locally tag everything as cached
     cat push.txt >>"${NYX_HOME}/cached.txt"
-
-    # Pin packages
-    if [ "${NYX_PIN:-}" = 'new' ] && [ -s to-pin.txt ]; then
-      cat to-pin.txt | xargs -n 2 niks3 pins create
-    elif [ "${NYX_PIN:-}" = 'full' ] && [ -s full-pin.txt ]; then
-      cat full-pin.txt | xargs -n 2 niks3 pins create
-    elif [ "${NYX_PIN:-}" = 'missing' ] && [ -s full-pin.txt ] && [ -s prev-cache.txt ]; then
-      comm -23 prev-cache.txt <(sort -u full-pin.txt) | xargs -rn 2 niks3 pins create
-    elif [ -n "${NYX_PIN:-}" ] && [ "$NYX_PIN" != 'none' ]; then
-      echo_error "Expected to pin, but some necessary file was missing or empty"
-      exit 69
-    fi
   else
-    echo_error "Nothing to push."
-    exit 42
+    echo_warning "Nothing to push."
+  fi
+
+  # Pin packages
+  if [ "${NYX_PIN:-}" = 'new' ] && [ -s to-pin.txt ]; then
+    cat to-pin.txt | xargs -n 2 niks3 pins create
+  elif [ "${NYX_PIN:-}" = 'full' ] && [ -s full-pin.txt ]; then
+    cat full-pin.txt | xargs -n 2 niks3 pins create
+  elif [ "${NYX_PIN:-}" = 'missing' ] && [ -s full-pin.txt ] && [ -s prev-cache.txt ]; then
+    comm -23 prev-cache.txt <(sort -u full-pin.txt) | xargs -rn 2 niks3 pins create
+  elif [ -n "${NYX_PIN:-}" ] && [ "$NYX_PIN" != 'none' ]; then
+    echo_warning "Expected to pin, but some necessary file was missing or empty"
   fi
 }
 
